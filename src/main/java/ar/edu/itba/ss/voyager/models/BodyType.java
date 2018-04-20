e19b8ea520b78cae9a43bdf2df2bd4072bc3e7f0
Mati <mfraga@itba.edu.ar>
2018-04-20 00:25:53 -0300

    Add body model

---
+++ src/main/java/ar/edu/itba/ss/voyager/models/BodyType.java
@@ -0,0 +1,8 @@
+package ar.edu.itba.ss.voyager.models;
+
+/**
+ * Describes all types of Bodys that could exist in this universe.
+ */
+public enum BodyType {
+    EARTH, SUN, MARS, SHIP, JUPITER, SATURN
+}
