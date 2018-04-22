01f4788cd23d0696fc838f13ef26488c600bae4b
Juan Marcos Bellini <juanmbellini@gmail.com>
2018-04-22 17:46:34 -0300

    Add planets' and sun's masses

--- src/main/java/ar/edu/itba/ss/voyager/models/Constants.java
+++ src/main/java/ar/edu/itba/ss/voyager/models/Constants.java
@@ -10,27 +10,22 @@ public class Constants {
      */
     public static final double G = 6.693 * Math.pow(10, -11);
 
-    // TODO: fill with real values
     /**
      * The Sun's mass (in kgs).
      */
-    public final static double SUN_MASS = 0;
-
+    public final static double SUN_MASS = 1.98855 * Math.pow(10, 30);
     /**
      * The Earth's mass (in kgs).
      */
-    public final static double EARTH_MASS = 0;
-
+    public final static double EARTH_MASS = 5.972 * Math.pow(10, 24);
     /**
      * Jupiter's mass (in kgs).
      */
-    public final static double JUPITER_MASS = 0;
-
+    public final static double JUPITER_MASS = 1.89813 * Math.pow(10, 27);
     /**
      * Saturn's mass (in kgs).
      */
-    public final static double SATURN_MASS = 0;
-
+    public final static double SATURN_MASS = 5.68319 * Math.pow(10, 26);
     /**
      * The Ship's mass (in kgs).
      */
